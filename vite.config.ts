e6e67cd36c43ee0167102080f69668f141188fde
donxavier-casa <don@auxoratech.com>
2025-08-08 11:42:10 +0530

    Fix path in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@src": path.resolve(__dirname, "./src"),
+      "@src": "/src",
     },
   },
   plugins: [react(), tailwindcss()],
